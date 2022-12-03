521b754deec17fea9e96cb01c76e420c4a458179
haiph1210 <haiph12102001@gmail.com>
2022-12-03 23:56:31 +0700

    YangKang

--- ProjectYangKang/YangKang/src/main/java/com/YangKang/payload/LoginResponse.java
+++ ProjectYangKang/YangKang/src/main/java/com/YangKang/payload/LoginResponse.java
@@ -1,13 +1,21 @@
 package com.YangKang.payload;
 
+import com.YangKang.entity.Account;
 import lombok.Data;
 
 @Data
 public class LoginResponse {
     private String accessToken;
     private String tokenType = "Bearer";
+    private String role ;
 
     public LoginResponse(String accessToken) {
         this.accessToken = accessToken;
+
     }
+
+//    public LoginResponse(String accessToken, Account.Role role) {
+//        this.accessToken = accessToken;
+//        this.role = role;
+//    }
 }
